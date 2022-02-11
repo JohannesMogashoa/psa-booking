c16f9c5b74a3bf2e9eb22b49258814a694cd07e6
Johannes Mogashoa <jomogashoa1993@gmail.com>
2022-02-11 23:06:17 +0200

    Added Formik + Yup

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,7 +3,7 @@ import GoogleProvider from "next-auth/providers/google"
 import { PrismaClient } from "@prisma/client";
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 
-const prisma = new PrismaClient()
+export const prisma = new PrismaClient()
 
 export default NextAuth({
     adapter: PrismaAdapter(prisma),
